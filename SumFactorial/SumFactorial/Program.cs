0f55937bffb95d7048c89a4eda924b8d4bb2c4a8
kumochan <thaotrinhminh@gmail.com>
2018-01-18 08:28:33 +0700

    init feature_dev

---
+++ SumFactorial/SumFactorial/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SumFactorial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var n = 5;
+            Console.WriteLine(SumFactorial(ref n));
+            Console.ReadLine();
+        }
+
+        static int SumFactorial(ref int i)
+        {
+            if (i <= 1)
+                return 1;
+
+            var n = i - 1;
+            return i * SumFactorial(ref n);
+        }
+    }
+}
